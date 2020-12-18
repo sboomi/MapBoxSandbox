186c3214a2de2af5bc4f892487ddc721664e0f7b
Shadi Boomi <shadi.boomi@gmail.com>
2020-12-18 12:37:03 +0100

    My First Commit

---
+++ frontend/script.js
@@ -0,0 +1,7 @@
+// To get an access token, log on mapboxgl
+mapboxgl.accessToken = 'mapbox-token-id-here';
+
+var map = new mapboxgl.Map({
+container: 'myMap',
+style: 'mapbox://styles/mapbox/streets-v11'
+});
